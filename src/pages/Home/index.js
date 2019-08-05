7ee2b4eb6ed26d0157a216f69d9cd524e80ef6e6
RBritoX <raphaellbrito@gmail.com>
2019-08-05 15:10:15 -0300

    atualização do projeto

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,10 +25,10 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -45,7 +45,7 @@ class Home extends Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <FaCartPlus size={16} color="#fff" />
@@ -72,7 +72,6 @@ const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  null,
   mapStateToProps,
   mapDispatchToProps
 )(Home);
